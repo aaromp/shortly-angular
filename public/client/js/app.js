26a6440d6a30a62f76ccfeb775a598c13b8d8f84
Hack Reactor Students <aaromp@gmail.com>
2014-05-16 20:54:35 -0700

    Added shortening capability and views

--- public/client/js/app.js
+++ public/client/js/app.js
@@ -1,6 +1,22 @@
-angular.module('Shortly',[])
+angular.module('Shortly',['ngRoute'])
 
-.controller('links', function($scope, $http) {
+.config(
+  function($routeProvider) {
+    $routeProvider
+      .when('/links', {
+        templateUrl: '/client/templates/home.html',
+        controller: 'ShowLinksController'
+      })
+      .when('/create', {
+        templateUrl: '/client/templates/shorten.html',
+        controller: 'ShortenLinkController'
+      })
+      .otherwise({
+        redirectTo: '/links'
+      });
+  })
+
+.controller('ShowLinksController', function($scope, $http) {
   $http({method: 'GET', url: '/links'})
     .success(function(data, status, headers, config) {
       $scope.links = data;
@@ -9,5 +25,19 @@ angular.module('Shortly',[])
       $scope.links = 'DANGER WILL ROBINSON';
       console.log(data, status, headers, config);
     });
-  $scope.thing = 'hello';
+})
+
+.controller('ShortenLinkController', function($scope, $http) {
+  $scope.submit = function() {
+    $http({
+      method: 'POST',
+      url: '/links',
+      // dataType: 'json',
+      data: JSON.stringify({url: $scope.url}),
+      headers : { 'Content-Type': 'application/json' }  // set the headers so angular passing info as form data (not request payload)
+    })
+    .success(function(data, status, headers, config) {
+      alert('You did it!');
+    });
+  };
 });
