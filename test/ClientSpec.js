75745f7e9f726af7978a46a21b24a1bf717b6eb7
Hack Reactor Students <aaromp@gmail.com>
2014-05-17 19:51:51 -0700

    begin work on tests

---
+++ test/ClientSpec.js
@@ -0,0 +1,14 @@
+describe('Client!', function() {
+
+  describe('ShortenLinkController', function() {
+    beforeEach(angular.module('Shortly'));
+
+    beforeEach(inject(function($injector){
+      rootScope = $injector.get('$rootScope');
+      $scope
+      controller = $injector.get('ShowLinksController');
+
+      makeController
+    });
+  });
+});
